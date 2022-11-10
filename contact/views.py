8570090dbe9b2c11307d550ef5670c1fdeeb512c
JBurrellIRL <jburrellirl@gmail.com>
2022-11-10 12:55:21 +0000

    Add test contact app

--- contact/views.py
+++ contact/views.py
@@ -11,12 +11,16 @@ def contact_view(request):
 		contact_form = ContactForm(request.POST)
 		if contact_form.is_valid():
 			contact_form.save()
-			messages.success(request, ('Your movie was successfully added!'))
+			# messages.success(request, ('Your query was successfully sent!'))
 		else:
 			messages.error(request, 'Error saving form')
 		
-		
-		return redirect("home")
+		return redirect("success")
 	contact_form = ContactForm()
 	contacts = Contact.objects.all()
-	return render(request=request, template_name="contact/contact.html", context={'contact_form': contact_form, 'contacts': contacts})
+	return render(request, "contact/contact.html", {'contact_form': contact_form})
+
+
+def success(request):
+    """A view to return the success page"""
+    return render(request, 'contact/success.html')
